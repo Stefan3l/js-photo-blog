7621b9a7bad653a177c4bddf63ef92ba3ebd6a78
Stefanel Mihaila <stefanel.mihaila@yahoo.com>
2025-01-09 17:07:39 +0100

    fix: add javascript in html

---
+++ script.js
@@ -0,0 +1,36 @@
+// DOM ELEMENTS
+
+const boxCardElm = document.getElementById('ms-box-card')
+
+
+    axios.get('https://jsonplaceholder.typicode.com/photos?_limit=6')
+        .then(function (response) {
+            
+            response.data.forEach(element => {
+                const resultTitle = element.title;
+                const resultUrl = element.url;
+
+                boxCardElm.innerHTML += 
+                (`<div class="col-sm col-md-6 col-lg-4">
+                        <div class="ms-card">
+                            <div class="pin">
+                                <img id="ms-image" src="./img/pin.svg">
+                            </div>
+                            <div> 
+                                <img class="ms-img-blog" src="${resultUrl}" alt="photo">
+                            </div>
+                            <div class="ms-title">
+                                <h2 id="ms-title-image">${resultTitle}</h2>
+                            </div>
+                        </div>               
+                </div>`)
+            })        
+    })
+        .catch(function()  {
+            boxCardElm.innerHTML += "404 Not Found"
+        });
+
+
+
+
+      

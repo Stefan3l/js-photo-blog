3a97e056b9f00d8568b6f50501d6d6bca6389c7c
Stefanel Mihaila <stefanel.mihaila@yahoo.com>
2025-01-10 09:40:55 +0100

    feat: efetto lightbox

--- script.js
+++ script.js
@@ -6,9 +6,10 @@ const boxCardElm = document.getElementById('ms-box-card')
     axios.get('https://jsonplaceholder.typicode.com/photos?_limit=6')
         .then(function (response) {
             
-            response.data.forEach(element => {
+            response.data.forEach((element, index) => {
                 const resultTitle = element.title;
                 const resultUrl = element.url;
+                const lightboxId = `lightbox-${index}`;
 
                 boxCardElm.innerHTML += 
                 (`<div class="col-12 col-md-6 col-lg-4 mt-5">
@@ -17,18 +18,18 @@ const boxCardElm = document.getElementById('ms-box-card')
                                 <img id="ms-image" src="./img/pin.svg">
                             </div>
                             <div> 
-                                <a href="#lightbox"><img class="ms-img-blog" src="${resultUrl}" alt="photo"></a>
+                                <a href="#${lightboxId}"><img class="ms-img-blog" src="${resultUrl}" alt="photo"></a>
                             </div>
                             <div class="ms-title">
                                 <h2 id="ms-title-image">${resultTitle}</h2>
                             </div>
                         </div>               
                 </div>
-                <div id="lightbox" class="lightbox">
+                <div id="${lightboxId}" class="lightbox">
                     <a href="#" class="lightbox-close">&times;</a>
                     <img src="${resultUrl}" alt="img">
                 </div>
-                `)
+                `);
             })        
     })
         .catch(function()  {
